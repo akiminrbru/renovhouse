9aa9b14de110fa9d577793f6e7fd358425a9c718
dev24 <dev24@macmini-dev24.local>
2023-06-23 13:00:56 +0300

    2 commit

--- src/js/import/slider.js
+++ src/js/import/slider.js
@@ -32,10 +32,28 @@ const swiper1 = new Swiper('.swiper1', {
 let swiper3 = new Swiper(".mySwiper", {
   loop: true,
   spaceBetween: 9,
-  slidesPerView: 6,
+  slidesPerView: 2,
   freeMode: true,
   watchSlidesProgress: true,
+  breakpoints: {
+    400: {
+        slidesPerView: 3,
+        spaceBetween: 9,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 9,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 9,
+    },
 
+    1200: {
+      slidesPerView: 6,
+      spaceBetween: 9,
+    },
+},
 });
 
 let swiper2 = new Swiper(".mySwiper2", {
